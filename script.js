318ab66679b91d0a0aa36338eec53259e783b359
Denis1099 <81426532+Denis1099@users.noreply.github.com>
2024-11-26 13:48:59 +0200

    Add basic sketch logic

---
+++ script.js
@@ -0,0 +1,18 @@
+const container = document.querySelector('#container');
+
+for(let i = 0; i < 16; i++){
+    for(let j = 0; j < 16; j++){
+        const div = document.createElement('div');
+        div.classList.add('grid-cell');
+        container.appendChild(div);
+
+    }
+}
+
+    const divs = document.querySelectorAll('.grid-cell')
+    divs.forEach(div => {
+        div.addEventListener('mouseover', ( ) => {
+            div.classList.add('color-change');
+    });
+    });
+
